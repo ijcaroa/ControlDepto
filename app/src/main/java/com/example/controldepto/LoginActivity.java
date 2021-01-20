7b5079927b1d101e08db6765f77121fdf9843dac
ignacio <ignaciojaviercaro@gmail.com>
2021-01-20 04:25:15 -0300

    commit 20-01-2020

--- app/src/main/java/com/example/controldepto/LoginActivity.java
+++ app/src/main/java/com/example/controldepto/LoginActivity.java
@@ -2,13 +2,49 @@ package com.example.controldepto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.controldepto.databinding.ActivityMainBinding;
 
 public class LoginActivity extends AppCompatActivity {
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.editTextMail.setText("Ingrese su mail");
+        mBinding.editTextPassword.setText("");
+
+        mBinding.buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String password = mBinding.editTextPassword.getText().toString();
+                String mail = mBinding.editTextMail.getText().toString();
+                Boolean pass = password.equals("123Pass") && mail.equals("ignacio@mail.cl");
+                if (!pass == true) {
+                    Toast.makeText(LoginActivity.this, "Ingreso Incorrecto", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(LoginActivity.this, "Ingreso Correcto", Toast.LENGTH_SHORT).show();
+                    passSecond();
+                }
+            }
+        });
+
     }
-}
+
+    public void passSecond(){
+
+        Intent intent = new Intent(LoginActivity.this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    }
+
