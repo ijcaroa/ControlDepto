ac8cb83855b8c12fbb8f0f7b698d9b2b02311ed1
ignacio <ignaciojaviercaro@gmail.com>
2021-01-22 01:37:36 -0300

    commit 21-01-2020/ AM

--- app/src/main/java/com/example/controldepto/SecondFragment.java
+++ app/src/main/java/com/example/controldepto/SecondFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +17,9 @@ import com.example.controldepto.databinding.FragmentSecondBinding;
 
 public class SecondFragment extends Fragment {
     private FragmentSecondBinding mBinding;
+    private boolean choice;
+    private int luces, dormitorio, cocina, baño, normal, regular, malo;
+    public int total;
     private String image, textProject, textunit;
 
     @Override
@@ -24,7 +29,9 @@ public class SecondFragment extends Fragment {
             image = getArguments().getString("clave1");
             textProject = getArguments().getString("clave2");
             textunit = getArguments().getString("clave3");
-        }
+
+
+         }
     }
 
     @Override
@@ -43,18 +50,37 @@ public class SecondFragment extends Fragment {
             mBinding.tVUnidad.setText(textunit);
             Glide.with(view).load(image).into(mBinding.iVBuild);
 
+            mBinding.rG1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+                    int index  = mBinding.rG1.indexOfChild(mBinding.rG1.findViewById(checkedId));
+                    if(index == 0){
+                        luces = 10;
+                    }else luces = 0;
+                    if (index == 1){
+                        dormitorio = 20;
+                    } else dormitorio = 0;
+                    if (index == 2){
+                        cocina = 30;
+                    }else cocina = 0;
+                    if (index == 3){
+                        baño = 40;
+                    }else baño = 0;
 
+                     int suma = luces + dormitorio + cocina + baño;
 
+                    mBinding.tVResultado.setText(""+ suma);
 
+                }
+            });
+            //mBinding.rG2.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+              //  @Override
+                //public void onCheckedChanged(RadioGroup group, int checkedId) {
+                  //  int index2 = mBinding.rG2.indexOfChild(mBinding.rG2.findViewById(checkedId));
+                //}
+            //});
+            
 
-
-/*
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
-            }
-        });*/
     }
+
 }
